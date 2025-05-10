c51a22c3eb9ff3f269e31d9d3bb12ceec61d6948
Priyanksu <140146085+Priyanksu28@users.noreply.github.com>
2025-05-10 18:47:40 +0530

    filter button in issue list admin dashboard

--- frontend/src/components/issue/Table.jsx
+++ frontend/src/components/issue/Table.jsx
@@ -57,6 +57,13 @@ const Table = () => {
       setFilteredIssues(data)
     }
 
+    const filterByButton = (status) => {
+      const data = issues.filter((issue) => 
+        issue.status.toLowerCase().includes(status.toLowerCase())
+      )
+      setFilteredIssues(data)
+    }
+
 
   return (
     <>
@@ -73,11 +80,21 @@ const Table = () => {
                 onChange={filterByChange}
             />
             <div>
-                <button className='px-2 py-1 bg-teal-600 text-white hover:bg-teal-700 mr-3'>Working...</button>
-                <button className='px-2 py-1 bg-teal-600 text-white hover:bg-teal-700 mr-3'>Approved</button>
-                <button className='px-2 py-1 bg-teal-600 text-white hover:bg-teal-700 mr-3'>Rejected</button>
-                <button className='px-2 py-1 bg-teal-600 text-white hover:bg-teal-700 mr-3'>Clear</button>
-                <button className='px-2 py-1 bg-teal-600 text-white hover:bg-teal-700 mr-3'>Pending</button>
+                <button 
+                  className='px-2 py-1 bg-teal-600 text-white hover:bg-teal-700 mr-3' 
+                  onClick={() => filterByButton('Working...')}>Working...</button>
+                <button 
+                  className='px-2 py-1 bg-teal-600 text-white hover:bg-teal-700 mr-3'
+                  onClick={() => filterByButton('Approved')}>Approved</button>
+                <button 
+                  className='px-2 py-1 bg-teal-600 text-white hover:bg-teal-700 mr-3' 
+                  onClick={() => filterByButton('Rejected')}>Rejected</button>
+                <button 
+                  className='px-2 py-1 bg-teal-600 text-white hover:bg-teal-700 mr-3' 
+                  onClick={() => filterByButton('Clear')}>Clear</button>
+                <button 
+                  className='px-2 py-1 bg-teal-600 text-white hover:bg-teal-700 mr-3' 
+                  onClick={() => filterByButton('Pending')}>Pending</button>
             </div>
             
           </div>
